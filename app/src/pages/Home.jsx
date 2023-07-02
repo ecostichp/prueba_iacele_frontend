463761e99a330183e32f8772ec0617820bd0590e
Lic. Emilio Costich <ecostichp@gmail.com>
2023-07-01 19:17:53 -0700

    Arreglé el padding start del home

--- app/src/pages/Home.jsx
+++ app/src/pages/Home.jsx
@@ -13,7 +13,7 @@ export default function Home() {
       </div>
     </header>
     <main>
-      <div className="mx-auto max-w-7xl py-6 sm:px-6 lg:px-8">
+      <div className="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
         <h1 className="text-2xl font-bold tracking-tight text-yellow-700 dark:text-yellow-600">Hola {user.name}</h1>
         <p className="text-gray-700 dark:text-gray-400 mt-2">Pronto vas a tener la mejor App de ventas...</p>
       </div>
