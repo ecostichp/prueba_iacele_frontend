389ad947e7a5345bf0fd8bf2092b49e8dc3671b9
Lic. Emilio Costich <ecostichp@gmail.com>
2023-07-01 20:30:05 -0700

    corregí el tamaño del botón de la página de ErrorPage

--- app/src/pages/ErrorPage.jsx
+++ app/src/pages/ErrorPage.jsx
@@ -34,7 +34,7 @@ export default function ErrorPage() {
       <div className="mt-12">
         <Link
           to='/'
-          className="rounded-md bg-indigo-600 dark:bg-indigo-500 px-3 py-1.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 dark:hover:bg-indigo-400 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600 dark:focus-visible:outline-indigo-500"
+          className="flex w-full justify-center rounded-md bg-indigo-600 dark:bg-indigo-500 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 dark:hover:bg-indigo-400 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600 dark:focus-visible:outline-indigo-500"
         >
           Sácame de aquí
         </Link>
